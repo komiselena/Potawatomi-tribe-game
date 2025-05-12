f707162ad41a4210ef686f3f020137740ed011f9
komiselena <komiselen@yandex.ru>
2025-05-12 13:32:26 +0300

    Initial Commit

---
+++ Potawatomi tribe gameTests/Potawatomi_tribe_gameTests.swift
@@ -0,0 +1,17 @@
+//
+//  Potawatomi_tribe_gameTests.swift
+//  Potawatomi tribe gameTests
+//
+//  Created by Mac on 12.05.2025.
+//
+
+import Testing
+@testable import Potawatomi_tribe_game
+
+struct Potawatomi_tribe_gameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Potawatomi tribe gameUITests/Potawatomi_tribe_gameUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Potawatomi_tribe_gameUITestsLaunchTests.swift
+//  Potawatomi tribe gameUITests
+//
+//  Created by Mac on 12.05.2025.
+//
+
+import XCTest
+
+final class Potawatomi_tribe_gameUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
